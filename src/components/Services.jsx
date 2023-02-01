13a376da6e4efa59a70b8cfd7bc23d60576d63e4
Jupiter Desphy <jupiterdesphy7@gmail.com>
2023-01-31 16:14:12 -0800

    improved responsiveness of service menu

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,56 +1,75 @@
 import React from 'react'
 
 export default function Services() {
-  return (
+    return (
 
-    <div className='services'>
-        {/* <div className="west-side"> */}
-        <h2>Services</h2>
-        {/* </div> */}
-        {/* <br /> */}
-        <div className="services-menu-container">
+        <div className='services'>
+            {/* <div className="west-side"> */}
+            <h1>Services</h1>
+            {/* </div> */}
+            {/* <br /> */}
 
-        <div className="services-menu">
+                <div className="services-menu">
 
 
-        {/* <div className=''> */}
-            <div>Haircut</div>
-            <div>$30</div>
-            <div>Long Haircut</div>
-            <div>$35</div>
-            <div>Buzz Cut</div>
-            <div>$20</div>
-        </div>
-        <div className="services-menu">
-            <div>Beard Trim</div>
-            <div>$15</div>
-            <div>Hot Towel Shave</div>
-            <div>$30</div>
-            <div>Haircut and Shave</div>
-            <div>$50</div>
-        </div>
-        <div className="services-menu">
-            <div>Haircut and Beard Trim</div>
-            <div>$40</div>
-            <div>Kids & Military Haircut</div>
-            <div>$25</div>
-            <div>Seniors Haircut</div>
-            <div>$22</div>
-        {/* </div> */}
-        {/* <div className=''> */}
-        {/* </div> */}
-        </div>
-        </div>
-            
-            
-            
-            
-            
-            
-            
-            
+                    {/* <div className=''> */}
+                    <div>
+                        <div>Haircut</div>
+                        <div>$30</div>
+                    </div>
+                    <div>
+                        <div>Long Haircut</div>
+                        <div>$35</div>
+                    </div>
+                    <div>
+                        <div>Buzz Cut</div>
+                        <div>$20</div>
+                    </div>
+                    <div>
+                        <div>Beard Trim</div>
+                        <div>$15</div>
+                    </div>
+                    <div>
+                        <div>Hot Towel Shave</div>
+                        <div>$30</div>
+                    </div>
+                    <div>
+                        <div>Haircut and Shave</div>
+                        <div>$50</div>
+                    </div>
+                    <div>
+                        <div>Haircut and Beard Trim</div>
+                        <div>$40</div>
+                    </div>
+                    <div>
+                        <div>Kids & Military Haircut</div>
+                        <div>$25</div>
+                    </div>
+                    <div>
+                        <div>Seniors Haircut</div>
+                        <div>$22</div>
+                    </div>
+                    <div></div>
+                    {/* </div>
+                </div>
+        <div className="services-menu"> */}
+                    {/* </div>
+        <div className="services-menu"> */}
+                    {/* </div> */}
+                    {/* <div className=''> */}
+                    {/* </div> */}
 
-        {/* <table>
+            </div>
+
+
+
+
+
+
+
+
+
+            {/* <table>
             <tbody>
             
                     Haircut
@@ -81,6 +100,6 @@ export default function Services() {
                     $22
             </tbody>
         </table> */}
-    </div>
-  )
+        </div>
+    )
 }
